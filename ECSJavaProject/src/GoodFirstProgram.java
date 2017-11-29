c3463d261a8250c5f418146049cd1aa66b4a1312
Anvaygoel <goelanvay@gmail.com>
2017-11-29 11:56:05 -0800

    ECS First Assignments
    Made my initials and speech wirtten out.

---
+++ ECSJavaProject/src/GoodFirstProgram.java
@@ -0,0 +1,16 @@
+
+public class GoodFirstProgram 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello again");
+		System.out.println("I like typing this");
+		System.out.println("This is fun");
+		System.out.println("Yay! printing.");
+		System.out.println("I'd mych rather you 'not'.");
+		System.out.println("I \"siad\"do not touch this");
+		//System.out.println("This is bob the builder, Yes we can!!!");
+	}
+
+}

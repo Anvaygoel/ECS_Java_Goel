abd4717065e7616fe9ecda1fbe2fa0c18836c44c
Anvaygoel <goelanvay@gmail.com>
2017-12-01 10:17:33 -0800

    Finsihed off the second set of assignments
    got all the variables and messages outputed.

---
+++ Variables/src/StillUsingVariables.java
@@ -0,0 +1,20 @@
+
+public class StillUsingVariables
+{
+	
+
+	public static void main(String[] args)
+	{
+		String yourname;
+		int yourgradeyear;
+		
+		yourname = "Anvay";
+		yourgradeyear = 2021;
+		
+		System.out.println("My name is " + yourname + " and I'll graduate in " + yourgradeyear + ".");
+		
+		
+		
+	}
+
+}
